a3ac677ad183439f9d50dfac5c2f8d8ba1b36f9a
cuidok <zapwe@qq.com>
2024-02-01 15:36:16 +0800

    Move component preview page to ComponentPreview.vue

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,11 @@
 <script setup>
 
+import ComponentPreview from "@/page/ComponentPreview.vue";
 </script>
 
 <template>
-  <div class="app__container">
-    <div class="app__menu">
-
-    </div>
-    <div class="app__body">
-
-    </div>
-  </div>
+  <component-preview></component-preview>
 </template>
 
 <style scoped>
-.app__container {
-  --menu-color: #d7d7d7;
-  --body-color: #dedede;
-}
-
-.app__container {
-  width: 98vw;
-  height: 98vh;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  flex-direction: row;
-}
-
-.app__menu {
-  width: 100px;
-  height: 100%;
-  margin-right: 10px;
-  background-color: var(--menu-color);
-}
-
-.app__body {
-  width: calc(100% - 100px);;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: var(--body-color);
-}
 </style>
---
+++ src/page/ComponentPreview.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="app__container">
+    <div class="app__menu">
+
+    </div>
+    <div class="app__body">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app__container {
+  --menu-color: #d7d7d7;
+  --body-color: #dedede;
+}
+
+.app__container {
+  width: 100%;
+  height: 98vh;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+}
+
+.app__menu {
+  width: 100px;
+  height: 100%;
+  margin-right: 10px;
+  background-color: var(--menu-color);
+}
+
+.app__body {
+  width: calc(100% - 100px);;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--body-color);
+}
+
+</style>
